aa94127658e0d3e06c9654e053fb6ced346f8b9b
Sumanth <sumanth.srinivasan@gmail.com>
2016-12-05 23:46:57 -0500

    Fixed code headers

--- main_script.js
+++ main_script.js
@@ -56,16 +56,13 @@ function initMap() {
   rectangle.addListener('bounds_changed', showNewRect);
 
   }
-=======
->>>>>>> d28b814a83b3506f74327713276c0d8cfa898f7d
 
 //Audio Objects
 var sMusic;
 var sTalking;
 var sTraffic;
 var sConstruction;
-      
-<<<<<<< HEAD
+
 function showNewRect(event) {
   var ne = rectangle.getBounds().getNorthEast();
   var sw = rectangle.getBounds().getSouthWest();
@@ -126,7 +123,6 @@ function AJAXcall(url) {
 
       var total = construction + loudMusic + loudTalking + traffic;
       console.log('Total ' + total);
-=======
 
 function initMap() {
 
@@ -173,11 +169,7 @@ function showNewRect(event) {
   var url = 'https://data.cityofnewyork.us/resource/fhrw-4uyv.json?$where=latitude > ' + sw.lat() + ' AND latitude < ' + ne.lat() + ' AND longitude > ' + sw.lng() + ' AND longitude < ' + ne.lng() + '&$$app_token=09sIcqEhoY0teGY5rhupZGqhW';
   // console.log(url);
   AJAXcall(url);
->>>>>>> d28b814a83b3506f74327713276c0d8cfa898f7d
 
-  
-
-<<<<<<< HEAD
       //Catch case of no complaints
       if (total == 0) {
 
@@ -215,7 +207,6 @@ function initAudio() {
     sMusic.player.loop = true;
    }).toMaster();
 
-=======
 }
 
 
@@ -273,10 +264,9 @@ function AJAXcall(url) {
       trafficGain = 20*Math.log10(traffic/total);
       // constructionGain = 
       
-<<<<<<< HEAD
+
       changeGain(musicGain,trafficGain, talkingGain);
-=======
->>>>>>> db6222b44bba3233be87511824a2b07c526d68cc
+
   });  
 }
 
@@ -296,20 +286,18 @@ function initAudio() {
 
    // console.log(sMusic);
 
-=======
     sMusic.triggerAttack(-1);
     sMusic.volume.value = -15;
     sMusic.loop.value = true;
    }).toMaster();
 
->>>>>>> db6222b44bba3233be87511824a2b07c526d68cc
->>>>>>> d28b814a83b3506f74327713276c0d8cfa898f7d
+
    
    sTraffic = new Tone.Sampler("./assets/traffic.wav", function() {
 
     sTraffic.triggerAttack(-1);
     sTraffic.volume.value = -15;
-<<<<<<< HEAD
+
     sTraffic.player.loop = true;
    }).toMaster();
 
@@ -337,12 +325,11 @@ function initAudio() {
 }
 
 function changeGain(musicGain, trafficGain, talkingGain, constructionGain) {
-=======
-<<<<<<< HEAD
+
     sTraffic.player.loop = true;
-=======
+
     sTraffic.loop.value = true;
->>>>>>> db6222b44bba3233be87511824a2b07c526d68cc
+
    }).toMaster();
 
    
@@ -350,21 +337,21 @@ function changeGain(musicGain, trafficGain, talkingGain, constructionGain) {
 
     sTalking.triggerAttack(-1);
     sTalking.volume.value = -15;
-<<<<<<< HEAD
+
     sTalking.player.loop = true;
-=======
+
     sTalking.loop.value = true;
->>>>>>> db6222b44bba3233be87511824a2b07c526d68cc
+
    }).toMaster(); 
 }
 
 function changeGain(musicGain, trafficGain, talkingGain) {
->>>>>>> d28b814a83b3506f74327713276c0d8cfa898f7d
+
 //Edit to add construction gain
 
   sMusic.volume.value = musicGain;
   sTraffic.volume.value = trafficGain;
-<<<<<<< HEAD
+
   sTalking.volume.value = talkingGain;
   sConstruction.volume.value = constructionGain-5;
   silence.volume.value = -40;
@@ -379,15 +366,11 @@ function zeroTotal() {
   sTalking.volume.value = -29;
   sConstruction.volume.value = -39;
   silence.volume.value = -10;
-=======
-<<<<<<< HEAD
+
   sTalking.volume.value = talkingGain;
-=======
+
   sTalking.volume.value = taling.Gain;
->>>>>>> db6222b44bba3233be87511824a2b07c526d68cc
 
-
->>>>>>> d28b814a83b3506f74327713276c0d8cfa898f7d
 }
 
 
